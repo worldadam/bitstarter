74dd5ca6ea77ddac1d58bc1445048d84dce0fc49
Adam Petersen <world.adam@gmail.com>
2013-07-12 05:13:11 +0000

    Added URL support

--- grader.js
+++ grader.js
@@ -24,6 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var sys = require('util');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,7 +68,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to Url')
         .parse(process.argv);
+    if(program.url) {
+        rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                sys.puts('Error: ' + result.message);
+            } else {
+                program.file = '.tmpFile';
+                fs.writeFileSync(program.file, result.toString());
+            }
+        });
+    }
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
