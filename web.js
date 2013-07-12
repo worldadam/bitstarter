cb2402b120f4f0db8a6757e096f4f556b1f36fc5
Adam Petersen <world.adam@gmail.com>
2013-07-12 00:36:58 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
